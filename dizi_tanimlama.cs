9a5b91a25803f43e96f78186b2c70e3ba207547b
kurukahve0 <94200224+kurukahve0@users.noreply.github.com>
2021-11-16 17:09:59 +0300

    Create dizi_tanimlama.cs

---
+++ dizi_tanimlama.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizi_tanımlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[3] = "mavi";
+            dizi[2] = 3;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(renkler[3]);
+            Console.WriteLine(dizi[2]);
+
+            Console.WriteLine("Dizinin uzunluğunu giriniz");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz:", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var item in sayiDizisi)
+            {
+                toplam += item;
+            }
+            Console.WriteLine("Sayının ortalaması: {0}", toplam / diziUzunlugu);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
