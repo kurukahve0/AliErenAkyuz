91bcf84e2fe3e201cd74bc5e8d49dcf583ba2f57
kurukahve0 <94200224+kurukahve0@users.noreply.github.com>
2021-11-17 00:05:22 +0300

    Create arry_sinif.cs

---
+++ arry_sinif.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arry_sınıf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 22, 17, 53, 99, 85, 77 };
+
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+            Array.Sort(sayiDizisi);
+            Console.WriteLine("*******Sıralı*******");
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+            Console.WriteLine("*******clear*******");
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+            Console.WriteLine("*******Ters*******");
+            Array.Reverse(sayiDizisi);
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+            Console.WriteLine("*******İndex*******");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 99));
+
+            Console.WriteLine("*******Resize*******");
+            Array.Resize<int>(ref sayiDizisi, 7);
+            sayiDizisi[6] = 876;
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
