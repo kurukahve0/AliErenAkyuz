71325a916f254de2ec9f3cf3398ab8f003496c6a
kurukahve0 <94200224+kurukahve0@users.noreply.github.com>
2021-11-17 20:53:24 +0300

    a

---
+++ metot_overloading.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi = "999";
+            bool a = int.TryParse(sayi, out int deger);
+            if (a)
+            {
+                Console.WriteLine("Başarılı\n{0}", deger);
+            }
+            else
+                Console.WriteLine("Başarısız");
+
+           
+
+            metotlar metot = new metotlar();
+
+            metot.Topla(5, 3, out int sonuc);
+
+
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+            Console.WriteLine("***********-");
+            int ifade = 999;
+            metot.EkranaYaz(ifade.ToString());
+            metot.EkranaYaz(ifade);
+            metot.EkranaYaz("Ali", "Eren",out string adSoyad);
+            Console.WriteLine(adSoyad);
+            Console.ReadKey();
+        }
+
+    }
+    class metotlar
+    {
+        public void Topla(int a, int b, out int Toplam)
+        {
+             Toplam = a + b;
+        }
+        public void EkranaYaz(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYaz(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYaz(string veri1,string veri2,out string adSoyad)
+        {
+            adSoyad = veri1 + " " + veri2;
+            Console.WriteLine(adSoyad);
+        }
+
+    }
+}
---
+++ metot_tanimlama.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod_tanimlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3;
+            Console.WriteLine(a + b);
+
+            Console.WriteLine(Topla(a, b));
+
+            Metodlar metod = new Metodlar();
+            metod.EkranaYaz(Topla(a, b).ToString());
+
+            int sonuc = metod.sonuc(ref a, ref b);
+            Console.WriteLine(sonuc);
+            Console.WriteLine(a + b);
+            Console.ReadKey();
+
+        }
+        static int Topla(int deger1,int deger2)
+        {
+            return deger1 + deger2;
+        }
+    }
+    class Metodlar
+    {
+        public void EkranaYaz(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public int sonuc(ref int deger1,ref int deger2)
+        {
+            deger1++;
+            deger2++;
+            return deger1 + deger2;
+
+
+        }
+    }
+}
