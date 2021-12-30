87e7c3aa03e803b3ae97cf4e6cbe6a7053961b7f
kurukahve0 <94200224+kurukahve0@users.noreply.github.com>
2021-12-30 15:08:58 +0300

    Create enum.cs

---
+++ enum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)gunler.Pazar);
+
+            int sicaklik = 27;
+
+            if (sicaklik < (int)havaDurumu.soguk)
+                Console.Write("hava soğuk çıkma");
+            else if (sicaklik > (int)havaDurumu.normal && sicaklik < (int)havaDurumu.sıcak)
+                Console.WriteLine("dışarı çıkmak için iyi bir hava");
+            else if (sicaklik > (int)havaDurumu.sıcak && sicaklik < (int)havaDurumu.cokSıcak)
+                Console.WriteLine("dışarı çıkmak için sıcak bir hava");
+            else if (sicaklik > (int)havaDurumu.cokSıcak)
+                Console.WriteLine("yapma yanarsın");
+            Console.ReadKey();
+        }
+    }
+
+    enum gunler
+    {
+    Pazartesi=1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+    }
+    enum havaDurumu
+    {
+        soguk=5,
+        normal=20,
+        sıcak=25,
+        cokSıcak=30
+    }
+}
